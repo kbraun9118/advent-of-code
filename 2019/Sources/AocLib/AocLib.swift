deb3afa9cf19498b2df4686aae5e0273f2d0ed83
Kurt Braun <kbraun9118@gmail.com>
2023-11-16 07:51:12 -0600

    Added file reader functions

--- 2019/Sources/AocLib/AocLib.swift
+++ 2019/Sources/AocLib/AocLib.swift
@@ -1,3 +1,12 @@
-public func hello() {
-    print("Hello from day 2")
+import Foundation
+
+public func readInputFile(_ day: String) -> [String] {
+  let file = try! String(contentsOfFile: "./input/\(day)/input.txt")
+  return file.split(whereSeparator: \.isNewline).compactMap(String.init)
+  }
+
+public func readTestFile(_ day: String) -> [String] {
+  let file = try! String(contentsOfFile: "./input/\(day)/test.txt")
+  return file.split(whereSeparator: \.isNewline).compactMap(String.init)
+
 }
--- 2019/Sources/day-01/main.swift
+++ 2019/Sources/day-01/main.swift
@@ -1,4 +1,6 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import Foundation
+import AocLib
 
-print("Hello, world!")
+let lines = readInputFile("day-01")
+
+print("\(lines)")
