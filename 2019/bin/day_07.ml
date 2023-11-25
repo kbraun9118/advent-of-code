62ec3be5955403bea04924e584849a4b22a9772b
Kurt Braun <kbraun@kbraun.dev>
2023-11-25 11:06:54 -0600

    Starting day 7

---
+++ 2019/bin/day_07.ml
@@ -0,0 +1,25 @@
+let combinations min max =
+  let nums = Seq.init (max - min + 1) (( + ) min) in
+  let rec combinations = function
+    | [ x ] -> [ [ x ] ]
+    | xs ->
+        List.map
+          (fun x ->
+            combinations (List.filter (( <> ) x) xs)
+            |> List.map (fun xs -> x :: xs))
+          xs
+        |> List.flatten
+  in
+  combinations (List.of_seq nums)
+
+let () =
+  let combos =
+    combinations 0 2 |> List.map (fun xs -> List.map string_of_int xs)
+  in
+  let combos =
+    List.map
+      (fun xs ->
+        List.fold_left (fun (n : string) (acc : string) -> n ^ acc) "" xs)
+      combos
+  in
+  List.iter print_endline combos
