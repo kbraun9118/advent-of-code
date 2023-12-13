4999d3eb55cca225085d433cad1dd24ee1cd3a68
Kurt Braun <kbraun@shelterinsurance.com>
2023-12-13 12:57:52 -0600

    Added Grid type to lib

--- 2023/src/bin/day-13/main.rs
+++ 2023/src/bin/day-13/main.rs
@@ -1,3 +1,24 @@
 fn main() {
-    println!("Hello");
+    let input = r#"#.##..##.
+..#.##.#.
+##......#
+##......#
+..#.##.#.
+..##..##.
+#.#.##.#."#
+        .lines()
+        .map(String::from)
+        .collect::<Vec<_>>();
+    let left = vec![1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+    for i in 1..left.len() {
+        println!(
+            "{:?}",
+            left[..i]
+                .iter()
+                .rev()
+                .zip(left[i..].iter())
+                .collect::<Vec<_>>()
+        );
+    }
 }
--- 2023/src/lib.rs
+++ 2023/src/lib.rs
@@ -1,4 +1,8 @@
-use std::fmt::Display;
+use core::fmt;
+use std::{
+    fmt::{Debug, Display},
+    ops::{Index, IndexMut},
+};
 
 pub fn read_input_lines(day: &'static str) -> Vec<String> {
     let file = std::fs::read_to_string(format!("./src/bin/day-{}/input.txt", day))
@@ -30,5 +34,207 @@ pub struct Coord<T> {
 impl<T> From<(T, T)> for Coord<T> {
     fn from((x, y): (T, T)) -> Self {
         Self { x, y }
+    }
+}
+
+pub struct Grid<T> {
+    height: usize,
+    width: usize,
+    grid: Vec<T>,
+}
+
+impl<T: Default> Grid<T> {
+    pub fn new(height: usize, width: usize) -> Self {
+        let mut grid = Vec::with_capacity(height * width);
+        for _ in 0..height * width {
+            grid.push(T::default());
+        }
+        Self {
+            height,
+            width,
+            grid,
+        }
+    }
+
+    pub fn height(&self) -> usize {
+        self.height
+    }
+
+    pub fn width(&self) -> usize {
+        self.width
+    }
+
+    pub fn index_iter<'a>(&'a self) -> impl Iterator<Item = Coord<usize>> + 'a {
+        self.grid
+            .iter()
+            .enumerate()
+            .map(|(i, _)| Coord::from((i % self.width, i / self.width)))
+    }
+
+    pub fn row(&self, index: usize) -> Vec<&T> {
+        let mut vec = vec![];
+        for x in 0..self.width {
+            vec.push(&self[(x, index)]);
+        }
+
+        vec
+    }
+    
+    pub fn column(&self, index: usize) -> Vec<&T> {
+        let mut vec = vec![];
+        for y in 0..self.height {
+            vec.push(&self[(index, y)]);
+        }
+
+        vec
+    }
+}
+
+impl<T> From<Vec<Vec<T>>> for Grid<T> {
+    fn from(value: Vec<Vec<T>>) -> Self {
+        let height = value.len();
+        let width = value[0].len();
+        let mut grid = Vec::with_capacity(height * width);
+        for row in value {
+            for item in row {
+                grid.push(item);
+            }
+        }
+
+        Self {
+            height,
+            width,
+            grid,
+        }
+    }
+}
+
+impl<T: Clone> Clone for Grid<T> {
+    fn clone(&self) -> Self {
+        Self {
+            height: self.height,
+            width: self.width,
+            grid: self.grid.clone(),
+        }
+    }
+}
+
+impl<T, E: Into<Coord<usize>>> Index<E> for Grid<T> {
+    type Output = T;
+
+    fn index(&self, index: E) -> &Self::Output {
+        let index = index.into();
+        assert!(index.x < self.width, "Cannot index past width");
+        assert!(index.y < self.height, "Cannot index past height");
+        &self.grid[index.y * self.width + index.x]
+    }
+}
+
+impl<T> IndexMut<Coord<usize>> for Grid<T> {
+    fn index_mut(&mut self, index: Coord<usize>) -> &mut Self::Output {
+        assert!(index.x < self.width, "Cannot index past width");
+        assert!(index.y < self.height, "Cannot index past height");
+        &mut self.grid[index.y * self.width + index.x]
+    }
+}
+
+impl<T: fmt::Debug> fmt::Debug for Grid<T> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let mut rows = vec![];
+        for y in 0..self.height {
+            rows.push((
+                y,
+                self.grid[y * self.width..y * self.width + self.width]
+                    .iter()
+                    .collect::<Vec<_>>(),
+            ));
+        }
+        f.debug_struct("Grid")
+            .field("height", &self.height)
+            .field("width", &self.width)
+            .field("grid", &rows)
+            .finish()
+    }
+}
+
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn grid_debug() {
+        let grid = Grid::from(vec![vec![1, 0], vec![0, 1]]);
+        let grid_debug = format!("{grid:?}");
+
+        assert_eq!(
+            grid_debug,
+            "Grid { height: 2, width: 2, grid: [(0, [1, 0]), (1, [0, 1])] }".to_string()
+        );
+    }
+
+    #[test]
+    fn indexing_grid() {
+        let grid = Grid::from(vec![vec![1, 2, 3], vec![4, 5, 6], vec![7, 8, 9]]);
+
+        assert_eq!(grid[(0, 0)], 1);
+        assert_eq!(grid[(0, 1)], 4);
+        assert_eq!(grid[(0, 2)], 7);
+        assert_eq!(grid[(1, 0)], 2);
+        assert_eq!(grid[(1, 1)], 5);
+        assert_eq!(grid[(1, 2)], 8);
+        assert_eq!(grid[(2, 0)], 3);
+        assert_eq!(grid[(2, 1)], 6);
+        assert_eq!(grid[(2, 2)], 9);
+    }
+
+    #[test]
+    fn grid_index_iter() {
+        let grid = Grid::from(vec![vec![1, 2, 3], vec![4, 5, 6], vec![7, 8, 9]]);
+        let mut index_iter = grid.index_iter();
+
+        assert_eq!(index_iter.next(), Some((0, 0).into()));
+        assert_eq!(index_iter.next(), Some((1, 0).into()));
+        assert_eq!(index_iter.next(), Some((2, 0).into()));
+        assert_eq!(index_iter.next(), Some((0, 1).into()));
+        assert_eq!(index_iter.next(), Some((1, 1).into()));
+        assert_eq!(index_iter.next(), Some((2, 1).into()));
+        assert_eq!(index_iter.next(), Some((0, 2).into()));
+        assert_eq!(index_iter.next(), Some((1, 2).into()));
+        assert_eq!(index_iter.next(), Some((2, 2).into()));
+        assert_eq!(index_iter.next(), None);
+    }
+
+    #[test]
+    fn test_index_throws_assert_errors() {
+        let grid = Grid::from(vec![vec![0]]);
+
+        let result = std::panic::catch_unwind(|| grid[(1, 0)]);
+        assert!(result.is_err());
+        let message: &str = *result.unwrap_err().downcast().unwrap();
+        assert_eq!(message, "Cannot index past width");
+
+        let result = std::panic::catch_unwind(|| grid[(0, 1)]);
+        assert!(result.is_err());
+        let message: &str = *result.unwrap_err().downcast().unwrap();
+        assert_eq!(message, "Cannot index past height");
+    }
+
+    #[test]
+    fn grid_row() {
+        let grid = Grid::from(vec![vec![1, 2, 3], vec![4, 5, 6], vec![7, 8, 9]]);
+
+        assert_eq!(grid.row(0), vec![&1, &2, &3]);
+        assert_eq!(grid.row(1), vec![&4, &5, &6]);
+        assert_eq!(grid.row(2), vec![&7, &8, &9]);
+    }
+
+    #[test]
+    fn grid_column() {
+        let grid = Grid::from(vec![vec![1, 2, 3], vec![4, 5, 6], vec![7, 8, 9]]);
+
+        assert_eq!(grid.column(0), vec![&1, &4, &7]);
+        assert_eq!(grid.column(1), vec![&2, &5, &8]);
+        assert_eq!(grid.column(2), vec![&3, &6, &9]);
     }
 }
