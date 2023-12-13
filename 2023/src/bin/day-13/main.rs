f9500b4b5e8cc8729889e76e82c1c63a23c01da1
Kurt Braun <kbraun9118@gmail.com>
2023-12-13 15:37:55 -0600

    Part1 complete

--- 2023/src/bin/day-13/main.rs
+++ 2023/src/bin/day-13/main.rs
@@ -1,6 +1,6 @@
 type Grid = aoc::Grid<Terrain>;
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 enum Terrain {
     Ash,
     Rock,
@@ -16,20 +16,81 @@ impl From<char> for Terrain {
     }
 }
 
+fn part_1(grids: &Vec<Grid>) -> usize {
+    let mut sum = 0;
+
+    for grid in grids {
+        if let Some(reflection) = (1..grid.width()).find(|i| {
+            (0..*i)
+                .into_iter()
+                .rev()
+                .zip(*i..grid.width())
+                .all(|(l, r)| grid.column(l) == grid.column(r))
+        }) {
+            sum += reflection;
+        } else if let Some(reflection) = (1..grid.height()).find(|i| {
+            (0..*i)
+                .into_iter()
+                .rev()
+                .zip(*i..grid.height())
+                .all(|(l, r)| grid.row(l) == grid.row(r))
+        }) {
+            sum += 100 * reflection;
+        }
+    }
+
+    sum
+}
+
 fn main() {
-    let input = r#"#.##..##.
+    let grids = aoc::read_input_lines("13")
+        .split(|l| l == "")
+        .map(|s| {
+            s.iter()
+                .map(|r| r.chars().map(Terrain::from).collect::<Vec<_>>())
+                .collect::<Vec<_>>()
+        })
+        .map(Grid::from)
+        .collect::<Vec<_>>();
+
+    aoc::print_part_1(part_1(&grids));
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    fn get_test_input() -> Vec<Grid> {
+         r#"#.##..##.
 ..#.##.#.
 ##......#
 ##......#
 ..#.##.#.
 ..##..##.
-#.#.##.#."#
-        .lines()
-        .map(String::from)
-        .map(|r| r.chars().map(Terrain::from).collect::<Vec<_>>())
-        .collect::<Vec<_>>();
+#.#.##.#.
 
-    let grid = Grid::new(input.len(), input[0].len(), |aoc::Coord {x, y}| input[y][x]);
+#...##..#
+#....#..#
+..##..###
+#####.##.
+#####.##.
+..##..###
+#....#..#"#
+            .lines()
+            .map(String::from)
+            .collect::<Vec<_>>()
+            .split(|l| l == "")
+            .map(|s| {
+                s.iter()
+                    .map(|r| r.chars().map(Terrain::from).collect::<Vec<_>>())
+                    .collect::<Vec<_>>()
+            })
+            .map(Grid::from)
+            .collect::<Vec<_>>()
+    }
 
-    println!("{grid:#?}");
+    #[test]
+    fn test_part_1() {
+        assert_eq!(part_1(&get_test_input()), 405);
+    }
 }
