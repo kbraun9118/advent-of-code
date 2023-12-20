3ab854519b5eb041c6ce3a5132a5cf90081501be
Kurt Braun <kbraun@shelterinsurance.com>
2023-12-20 15:38:30 -0600

    Started work on day 20

---
+++ 2023/src/bin/day-20/main.rs
@@ -0,0 +1,50 @@
+#[derive(Debug, Copy, Clone, PartialEq)]
+enum Pulse {
+    High,
+    Low,
+}
+#[derive(Debug, Copy, Clone, PartialEq)]
+enum State {
+    On,
+    Off,
+}
+
+#[derive(Debug, Clone)]
+struct FlipFlop {
+    state: State,
+    output: Vec<String>,
+}
+
+impl FlipFlop {
+    fn receive(&mut self, pulse: Pulse) -> (Pulse, Vec<String>) {
+        match pulse {
+            Pulse::High => (pulse, vec![]),
+            Pulse::Low => match self.state {
+                State::On => {
+                    self.state = State::Off;
+                    (Pulse::Low, self.output.clone())
+                }
+                State::Off => {
+                    self.state = State::On;
+                    (Pulse::High, self.output.clone())
+                }
+            },
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+struct Conjunction {
+    previous_pulse: Pulse,
+    output: Vec<String>,
+}
+
+#[derive(Debug, Clone)]
+enum Module {
+    FlipFlop(FlipFlop),
+    Conjunction(Conjunction),
+    Broadcaster(Vec<String>),
+}
+fn main() {
+    println!("Hello");
+}
