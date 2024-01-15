d5909ae4189e49f79b500a70db3b9d4fc6a3536c
Kurt Braun <kbraun9118@gmail.com>
2024-01-14 21:55:25 -0600

    Finished day 1

--- 2016/day-01.py
+++ 2016/day-01.py
@@ -32,7 +32,7 @@ def part1(directions: list[Directions]) -> int:
 def part2(directions: list[Directions]) -> int:
     x, y = 0, 0
     facing = 0
-    visited: set[tuple[int, int]] = set()
+    visited = set()
     for direction in directions:
         dist = direction["dist"]
         match direction["dir"]:
@@ -40,24 +40,25 @@ def part2(directions: list[Directions]) -> int:
                 facing = (facing - 1) % 4
             case "R":
                 facing = (facing + 1) % 4
-        match facing:
-            case 0:
-                y -= dist
-            case 1:
-                x += dist
-            case 2:
-                y += dist
-            case 3:
-                x -= dist
-        if (x, y) in visited:
-            return abs(x) + abs(y)
-        visited.add((x, y))
+        for _ in range(dist):
+            match facing:
+                case 0:
+                    y -= 1
+                case 1:
+                    x += 1
+                case 2:
+                    y += 1
+                case 3:
+                    x -= 1
+            if (x, y) in visited:
+                return abs(x) + abs(y)
+            visited.add((x, y))
     return abs(x) + abs(y)
 
 
 if __name__ == "__main__":
-    # lines = lib.read_input_file("01")[0].split(", ")
-    lines = "R8, R4, R4, R8".split(", ")
+    lines = lib.read_input_file("01")[0].split(", ")
+    # lines = "R8, R4, R4, R8".split(", ")
     directions: list[Directions] = [
         {"dir": line[:1], "dist": int(line[1:])} for line in lines
     ]
