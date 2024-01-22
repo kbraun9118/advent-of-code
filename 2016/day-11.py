a63bdfde0745cab1da05071c6001dfeab6a3f1bc
Kurt Braun <kbraun9118@gmail.com>
2024-01-22 10:50:00 -0600

    added classes

--- 2016/day-11.py
+++ 2016/day-11.py
@@ -1,6 +1,21 @@
+from enum import Enum
 import lib
+
+class Type(Enum):
+    Generator = 0
+    Microchip = 1
+
+    
+class Component:
+    def __init__(self, type: Type, value: str) -> None:
+        self.type = type
+        self.value = value
+
+    def __str__(self) -> str:
+        return f"Component(type={self.type}, value={self.value})"
+    
 
 if __name__ == "__main__":
     lines = lib.read_input_file("11")
-    for line in lines:
-        print(line)
+    component = Component(Type.Generator, "hydrogen")
+    print(component)
