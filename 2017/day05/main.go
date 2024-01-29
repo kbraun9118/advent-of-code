d8a6a91e1e3dcbc5e25bfcaa65c671ed1d214ecf
Kurt Braun <kbraun@shelterinsurance.com>
2024-01-29 07:54:29 -0600

    day 5 complete

--- 2017/day05/main.go
+++ 2017/day05/main.go
@@ -2,8 +2,39 @@ package main
 
 import (
 	"aoc/2017/lib"
+	"slices"
 	"strconv"
 )
+
+func part1(instructions []int) int {
+  i, count := 0, 0
+  for i < len(instructions) {
+    current := i + instructions[i]
+    instructions[i]++
+    count++
+    i = current
+  }
+
+  return count
+}
+
+func part2(instructions []int) int {
+  i, count := 0, 0
+  for i < len(instructions) {
+    
+    current := i + instructions[i]
+    if instructions[i] > 2 {
+      instructions[i]--
+    } else {
+      instructions[i]++
+    }
+    count++
+    i = current
+  }
+
+  return count
+
+}
 
 func main() {
 	lines := lib.ReadInputFile("05")
@@ -12,5 +43,6 @@ func main() {
     return instruction
   })
 
-	lib.PrintPart1(instructions)
+	lib.PrintPart1(part1(slices.Clone(instructions)))
+  lib.PrintPart2(part2(instructions))
 }
