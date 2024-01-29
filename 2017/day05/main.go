ceeae6acee687e8431ed95660cd8de107ecabdef
Kurt Braun <kbraun@kbraun.dev>
2024-01-29 07:43:35 -0600

    Adding day 5 files

--- 2017/day05/main.go
+++ 2017/day05/main.go
@@ -1,9 +1,16 @@
 package main
 
-import "aoc/2017/lib"
+import (
+	"aoc/2017/lib"
+	"strconv"
+)
 
 func main() {
 	lines := lib.ReadInputFile("05")
+  instructions := lib.Map(lines, func(line string) int {
+    instruction, _ := strconv.Atoi(line)
+    return instruction
+  })
 
-	lib.PrintPart1(lines)
+	lib.PrintPart1(instructions)
 }
--- 2017/lib/lib.go
+++ 2017/lib/lib.go
@@ -6,6 +6,14 @@ import (
 	"strings"
 )
 
+func Map[T any, V any](values []T, mapper func(t T) V) []V {
+	vs := make([]V, len(values))
+	for i, value := range values {
+		vs[i] = mapper(value)
+	}
+	return vs
+}
+
 func ReadInputFile(day string) []string {
 	content, err := os.ReadFile(fmt.Sprintf("../input/2017/%s.txt", day))
 	if err != nil {
