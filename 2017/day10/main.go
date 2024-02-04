36c3292813302c323cd9a8dfd5c06597e47f330c
Kurt Braun <kbraun@kbraun.dev>
2024-02-04 15:09:07 -0600

    Moved KnotHash to lib in prep for day 14

--- 2017/day10/main.go
+++ 2017/day10/main.go
@@ -2,43 +2,14 @@ package main
 
 import (
 	"aoc/2017/lib"
-	"fmt"
 	"strconv"
 	"strings"
 )
 
-type RingBuffer struct {
-	buffer []int
-	length int
-}
-
-func (rb *RingBuffer) realIndex(index int) int {
-	return index % rb.length
-}
-
-func (rb *RingBuffer) ReverseRange(start, end int) {
-	for i := 0; i < (end-start)/2; i++ {
-		startI := rb.realIndex(i + start)
-		endI := rb.realIndex(end - i - 1)
-		rb.buffer[startI], rb.buffer[endI] = rb.buffer[endI], rb.buffer[startI]
-	}
-}
-
-func CreateRingBuffer(length int) RingBuffer {
-	buffer := make([]int, length)
-	for i := range buffer {
-		buffer[i] = i
-	}
-	return RingBuffer{
-		buffer,
-		length,
-	}
-}
-
 func part1(inputs []int, bufferLength int) int {
 	position := 0
 	skip := 0
-	list := CreateRingBuffer(bufferLength)
+	list := lib.CreateRingBuffer(bufferLength)
 
 	for _, length := range inputs {
 		list.ReverseRange(position, position+length)
@@ -46,45 +17,11 @@ func part1(inputs []int, bufferLength int) int {
 		skip++
 	}
 
-	return list.buffer[0] * list.buffer[1]
+	return list.Score()
 }
 
 func part2(input string) string {
-	position := 0
-	skip := 0
-	list := CreateRingBuffer(256)
-	inputAscii := make([]int, 0)
-	for _, ch := range input {
-		inputAscii = append(inputAscii, int(ch))
-	}
-	endSeq := []int{17, 31, 73, 47, 23}
-	for _, i := range endSeq {
-		inputAscii = append(inputAscii, i)
-	}
-
-	for i := 0; i < 64; i++ {
-		for _, length := range inputAscii {
-			list.ReverseRange(position, position+length)
-			position += length + skip
-			skip++
-		}
-	}
-	denseHash := make([]int, 16)
-
-	for i := 0; i < 16; i++ {
-		denseHash[i] = 0
-		for j := 0; j < 16; j++ {
-			denseHash[i] ^= list.buffer[i*16+j]
-		}
-	}
-
-	output := ""
-
-	for _, out := range denseHash {
-		output += fmt.Sprintf("%0.2x", out)
-	}
-
-	return output
+	return lib.KnotHash(input)
 }
 
 func main() {
--- 2017/lib/lib.go
+++ 2017/lib/lib.go
@@ -43,3 +43,74 @@ func PrintPart1(output any) {
 func PrintPart2(output any) {
 	fmt.Printf("Part 2: %+v\n", output)
 }
+
+type RingBuffer struct {
+	buffer []int
+	length int
+}
+
+func (rb *RingBuffer) realIndex(index int) int {
+	return index % rb.length
+}
+
+func (rb *RingBuffer) ReverseRange(start, end int) {
+	for i := 0; i < (end-start)/2; i++ {
+		startI := rb.realIndex(i + start)
+		endI := rb.realIndex(end - i - 1)
+		rb.buffer[startI], rb.buffer[endI] = rb.buffer[endI], rb.buffer[startI]
+	}
+}
+
+func (rb RingBuffer) Score() int {
+  return rb.buffer[0] * rb.buffer[1]
+}
+
+func CreateRingBuffer(length int) RingBuffer {
+	buffer := make([]int, length)
+	for i := range buffer {
+		buffer[i] = i
+	}
+	return RingBuffer{
+		buffer,
+		length,
+	}
+}
+
+func KnotHash(input string) string {
+	position := 0
+	skip := 0
+	list := CreateRingBuffer(256)
+	inputAscii := make([]int, 0)
+	for _, ch := range input {
+		inputAscii = append(inputAscii, int(ch))
+	}
+	endSeq := []int{17, 31, 73, 47, 23}
+	for _, i := range endSeq {
+		inputAscii = append(inputAscii, i)
+	}
+
+	for i := 0; i < 64; i++ {
+		for _, length := range inputAscii {
+			list.ReverseRange(position, position+length)
+			position += length + skip
+			skip++
+		}
+	}
+	denseHash := make([]int, 16)
+
+	for i := 0; i < 16; i++ {
+		denseHash[i] = 0
+		for j := 0; j < 16; j++ {
+			denseHash[i] ^= list.buffer[i*16+j]
+		}
+	}
+
+	output := ""
+
+	for _, out := range denseHash {
+		output += fmt.Sprintf("%0.2x", out)
+	}
+
+	return output
+
+}
