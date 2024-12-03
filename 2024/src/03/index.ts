485405a8a1238e049cb97f031d866b97d3b820a6
Kurt Braun <kbraun9118@gmail.com>
2024-12-03 14:10:16 -0600

    need to debug

--- 2024/src/03/index.ts
+++ 2024/src/03/index.ts
@@ -1,25 +1,35 @@
 import util from "../util";
 
-const mulPattern = /mul\(\d{1,3},\d{1,3}\)/;
+const mulPattern = /(mul\((\d{1,3}),(\d{1,3})\))|(do\(\))|(don't\(\))/g;
 
-function sumForLine(line: string): number {
-  const matches =  mulPattern.exec(line);
+function sumForLine(line: string, enableDisable: boolean): number {
+  let matches = line.matchAll(mulPattern);
 
-  if (!matches) {
-    return 0;
-  }
   let sum = 0;
+  let enabled = true;
 
   for (const match of matches) {
-    console.log(match)
-    sum += 1;
+
+    if (enableDisable) {
+      if (match[0].startsWith("don't")) {
+        enabled = false;
+      }
+      if (match[0].startsWith("do(")) {
+        enabled = true;
+      }
+    }
+
+    if (enabled && match[0].startsWith("mul")) {
+      sum += +match[2] * +match[3];
+    }
   }
 
 return sum;
 }
 
-const input = util.readInput("03", true);
+const input = util.readInput("03");
 
-const part1 = input.map(sumForLine).reduce((acc, curr) => acc + curr);
+const part1 = input.map(line => sumForLine(line, false)).reduce((acc, curr) => acc + curr);
+const part2 = input.map(line => sumForLine(line, true)).reduce((acc, curr) => acc + curr);
 
-util.writeOutput(part1);
+util.writeOutput(part1, part2);
