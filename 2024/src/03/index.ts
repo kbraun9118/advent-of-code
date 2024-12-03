a03723f9f67a0f82369dec7e83a8aea6a2050222
Kurt Braun <kbraun9118@gmail.com>
2024-12-03 15:02:40 -0600

    2024/03

--- 2024/src/03/index.ts
+++ 2024/src/03/index.ts
@@ -1,8 +1,8 @@
 import util from "../util";
 
-const mulPattern = /(mul\((\d{1,3}),(\d{1,3})\))|(do\(\))|(don't\(\))/g;
+const mulPattern = /((mul\((\d{1,3}),(\d{1,3})\))|(do\(\))|(don't\(\)))/g;
 
-function sumForLine(line: string, enableDisable: boolean): number {
+function sumProgram(line: string, enableDisable: boolean): number {
   let matches = line.matchAll(mulPattern);
 
   let sum = 0;
@@ -20,16 +20,16 @@ function sumForLine(line: string, enableDisable: boolean): number {
     }
 
     if (enabled && match[0].startsWith("mul")) {
-      sum += +match[2] * +match[3];
+      sum += +match[3] * +match[4];
     }
   }
 
 return sum;
 }
 
-const input = util.readInput("03");
+const input = util.readInput("03").join();
 
-const part1 = input.map(line => sumForLine(line, false)).reduce((acc, curr) => acc + curr);
-const part2 = input.map(line => sumForLine(line, true)).reduce((acc, curr) => acc + curr);
+const part1 = sumProgram(input, false)
+const part2 = sumProgram(input, true)
 
 util.writeOutput(part1, part2);
