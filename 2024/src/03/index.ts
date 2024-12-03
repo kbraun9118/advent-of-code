8f9ab94a6dcca983b28a554251428ff707115b2a
Kurt Braun <kbraun@kbraun.dev>
2024-12-03 13:31:51 -0600

    wip

--- 2024/src/03/index.ts
+++ 2024/src/03/index.ts
@@ -1,5 +1,25 @@
 import util from "../util";
 
-const input = util.readInput("03");
+const mulPattern = /mul\(\d{1,3},\d{1,3}\)/;
 
-util.writeOutput(input.join('\n'));
+function sumForLine(line: string): number {
+  const matches =  mulPattern.exec(line);
+
+  if (!matches) {
+    return 0;
+  }
+  let sum = 0;
+
+  for (const match of matches) {
+    console.log(match)
+    sum += 1;
+  }
+
+return sum;
+}
+
+const input = util.readInput("03", true);
+
+const part1 = input.map(sumForLine).reduce((acc, curr) => acc + curr);
+
+util.writeOutput(part1);
