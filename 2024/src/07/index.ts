447a6d2c3c5475371da2f51edbc441acf788525d
Kurt Braun <kbraun@kbraun.dev>
2024-12-07 14:01:55 -0600

    2024/07 complete

--- 2024/src/07/index.ts
+++ 2024/src/07/index.ts
@@ -1,5 +1,45 @@
 import util from "../util";
 
-const input = util.readInput("07", true);
+function containsTarget(
+  equation: number[],
+  target: number,
+  useConcate: boolean = false,
+): boolean {
+  let previous = [equation[0]];
+  for (let i = 1; i < equation.length; i++) {
+    const current = [];
+    for (let previousNum of previous) {
+      const addition = previousNum + equation[i];
+      const multiplicaiton = previousNum * equation[i];
+      if (addition <= target) {
+        current.push(addition);
+      }
+      if (multiplicaiton <= target) {
+        current.push(multiplicaiton);
+      }
+      if (useConcate) {
+        const concatenation = +(`${previousNum}` + equation[i]);
+        if (concatenation <= target) {
+          current.push(concatenation);
+        }
+      }
+    }
+    previous = current;
+  }
 
-util.writeOutput(input.join("\n"));
+  return previous.includes(target);
+}
+
+const input: [number, number[]][] = util
+  .readInput("07")
+  .map((line) => line.split(": "))
+  .map(([target, equation]) => [+target, equation.split(" ").map((e) => +e)]);
+
+const part1 = input
+  .filter(([target, equation]) => containsTarget(equation, target))
+  .reduce((acc, [target]) => acc + target, 0);
+const part2 = input
+  .filter(([target, equation]) => containsTarget(equation, target, true))
+  .reduce((acc, [target]) => acc + target, 0);
+
+util.writeOutput(part1, part2);
