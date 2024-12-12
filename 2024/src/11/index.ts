445c122f77df6921ac48c76f4344d76b9cb8c3a9
Kurt Braun <kbraun@shelterinsurance.com>
2024-12-12 07:45:05 -0600

    2024/11 complete

--- 2024/src/11/index.ts
+++ 2024/src/11/index.ts
@@ -16,19 +16,61 @@ function blink(stones: number[]): number[] {
   return output;
 }
 
-function blinkTimes(stones: number[]): [ number, number ] {
-  let times25 = 0;
-  for (let i = 0; i < 75; i++) {
-    console.log(i, stones.length);
-    stones = blink(stones);
-    if (i==24) {
-      times25 = stones.length
-    }
+function stoneBlinksTo(
+  stone: number,
+  timesToExpand: number,
+  stoneTimesMap: Map<string, number> = new Map(),
+): number {
+  if (timesToExpand === 0) {
+    return 1;
   }
-  return [times25, stones.length];
+  const stoneTimesString = `${stone},${timesToExpand}`;
+  if (stoneTimesMap.has(stoneTimesString)) {
+    return stoneTimesMap.get(stoneTimesString)!;
+  }
+  let expanded: number;
+  if (stone === 0) {
+    expanded = stoneBlinksTo(1, timesToExpand - 1, stoneTimesMap);
+  } else if (`${stone}`.length % 2 === 0) {
+    const stoneString = `${stone}`;
+
+    expanded =
+      stoneBlinksTo(
+        +stoneString.slice(0, stoneString.length / 2),
+        timesToExpand - 1,
+        stoneTimesMap,
+      ) +
+      stoneBlinksTo(
+        +stoneString.slice(stoneString.length / 2),
+        timesToExpand - 1,
+        stoneTimesMap,
+      );
+  } else {
+    expanded = stoneBlinksTo(stone * 2024, timesToExpand - 1, stoneTimesMap);
+  }
+  stoneTimesMap.set(stoneTimesString, expanded);
+
+  return expanded;
+}
+
+function blinkTimes(stones: number[]): number {
+  let times25 = 0;
+  for (const stone of stones) {
+    let currentStones = [stone];
+    for (let i = 0; i < 25; i++) {
+      currentStones = blink(currentStones);
+    }
+    times25 += currentStones.length;
+  }
+  return times25;
 }
 
 const input = util.readInput("11")[0];
 const stones = input.split(" ").map((s) => +s);
 
-util.writeOutput(...blinkTimes(stones));
+let part2 = 0;
+for (const stone of stones) {
+  part2 += stoneBlinksTo(stone, 75);
+}
+
+util.writeOutput(blinkTimes(stones), part2);
