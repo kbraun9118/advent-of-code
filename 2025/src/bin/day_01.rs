1b121f9b81068dfe0b465d77d00e73b84a21337d
Kurt Braun <kbraun@kbraun.dev>
2025-12-01 10:47:48 -0600

    2025/01 complete

---
+++ 2025/src/bin/day_01.rs
@@ -0,0 +1,37 @@
+use aoc_2025::{Error, print_output, read_input};
+
+fn part_solve(input: Vec<String>) -> Result<(usize, usize), Error> {
+    let mut current = 50;
+    let (mut part_1, mut part_2) = (0, 0);
+
+    for ref mut i in input {
+        let sign = match i {
+            _ if i.starts_with("L") => -1,
+            _ if i.starts_with("R") => 1,
+            value => return Err(Error::from(format!("Invalid variant: {}", value))),
+        };
+        let offset: usize = i.split_off(1).parse()?;
+
+        for _ in 0..offset {
+            current += sign;
+            current = ((current % 100) + 100) % 100;
+            if current == 0 {
+                part_2 += 1;
+            }
+        }
+        if current == 0 {
+            part_1 += 1;
+        }
+    }
+    Ok((part_1, part_2))
+}
+
+fn main() -> Result<(), Error> {
+    let input = read_input("01")?;
+
+    let (part_1, part_2) = part_solve(input)?;
+
+    print_output!(part_1, part_2);
+
+    Ok(())
+}
